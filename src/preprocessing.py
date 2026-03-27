f552e73085d267b26b2426968f43647e8d1b799c
Sirius-Siru <haudu12343@gmail.com>
2026-03-27 14:34:56 +0700

    push

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -29,13 +29,33 @@ def augment(img):
     return cv2.warpAffine(img, M, (28, 28), borderValue=0)
 
 def skltn(img):
-    img_bin = (img > 0.5).astype(np.uint8)
+    img_bin = (img > 0.7).astype(np.uint8)
     skeleton = skeletonize(img_bin)
     return skeleton
+
+def getDensityRatio(img){
+    rows = np.any(img > 0, axis=1)
+    cols = np.any(img > 0, axis=0)
+    ymin, ymax = np.where(rows)[0][[0, -1]]
+    xmin, xmax = np.where(cols)[0][[0, -1]]
+
+    cropped = img[ymin:ymax+1, xmin:xmax+1]
+
+    height = cropped.shape[0]
+    upper_half = cropped[:height//2, :]
+    lower_half = cropped[height//2:, :]
+    
+    density_ratio = np.sum(upper_half) / (np.sum(lower_half) + 1e-6)
+    return density_ratio
+}
 
 def aug_skltn_e_hog(img, is_train):
     # Augment
     aug = augment(img) if is_train else None 
+
+    # Calculate half upper and half lower ratio
+    origin_DRatio = getDensityRatio(img)
+    aug_DRatio = getDensityRatio(aug) if is_train else None
 
     # Skeletonize
     sklt = skltn(img)
@@ -46,8 +66,8 @@ def aug_skltn_e_hog(img, is_train):
     hog_aug_origin = extract_hog(aug) if is_train else None
 
     # Combine
-    orgin_combined = np.hstack((img.flatten(), hog_origin, sklt.flatten()))
-    aug_combined = np.hstack((aug.flatten(), hog_aug_origin, aug_sklt.flatten())) if is_train else None
+    orgin_combined = np.hstack((img.flatten(), hog_origin, sklt.flatten(), origin_DRatio))
+    aug_combined = np.hstack((aug.flatten(), hog_aug_origin, aug_sklt.flatten(), aug_DRatio)) if is_train else None
 
     return np.vstack((orgin_combined, aug_combined)) if is_train else orgin_combined
 
